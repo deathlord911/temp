b97b0cc204f51d71d6c74dba3d1748a3cf09134a
root <root@pve3.amazonistan.intranet>
2025-11-01 15:27:06 +0100

    feat: Playbook 05 – Cross-DNS, Samba forwarder, SSH Keys for SYSVOL, Health report

--- scripts/run_playbooks.sh
+++ scripts/run_playbooks.sh
@@ -6,3 +6,4 @@ ansible-playbook -i "$INV" ansible/playbooks/01_destroy_old_dcs.yml || true
 ansible-playbook -i "$INV" ansible/playbooks/02_create_dc_configs.yml
 ansible-playbook -i "$INV" ansible/playbooks/03_install_dcs_with_toolbox.yml
 ansible-playbook -i "$INV" ansible/playbooks/04_upgrade_and_check.yml
+ansible-playbook -i ansible/hosts ansible/playbooks/05_cross_dns_and_sysvol.yml || exit 1
